a61f68211e75e1c864310be558a62655f7a21740
magicM00nchild <dorit.wanner@gmx.de>
2024-09-08 23:22:22 +0200

    add prettier and pull things out of the tile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import CarouselTileList from './CarouselList/CarouselTileList'
+import CarouselTileList from './CarouselList/CarouselTileList';
 
 function App() {
   return (

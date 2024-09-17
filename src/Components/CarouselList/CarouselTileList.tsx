1a0bedcfa7bf5dcc685fcb7dba1eacf6fadc7ef5
magicM00nchild <dorit.wanner@gmx.de>
2024-09-17 22:54:44 +0200

    clean up code

--- src/Components/CarouselList/CarouselTileList.tsx
+++ src/Components/CarouselList/CarouselTileList.tsx
@@ -11,36 +11,6 @@ export interface TileContent {
 }
 
 const CarouselTileList = ({ tiles }: { tiles: TileContent[] }) => {
-  /*
-  const tiles: TileContent[] = [
-    {
-      id: 0,
-      content: { title: '0', info: 'test2' },
-      imageSrc: require('../../Pictures/images.png'),
-    },
-    {
-      id: 1,
-      content: { title: '1', info: 'test2' },
-      imageSrc: require('../../Pictures/images.png'),
-    },
-    {
-      id: 2,
-      content: { title: '2', info: 'test2' },
-      imageSrc: require('../../Pictures/images.png'),
-    },
-    {
-      id: 3,
-      content: { title: '3', info: 'test2' },
-      imageSrc: require('../../Pictures/images.png'),
-    },
-    {
-      id: 4,
-      content: { title: '4', info: 'test2' },
-      imageSrc: require('../../Pictures/images.png'),
-    },
-  ];
-  */
-
   const [currentIndex, setCurrentIndex] = useState(1);
 
   const handleNext = () => {
